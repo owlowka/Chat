7ba908944c3211ed329ad4a2fcfc99f9a26c5ea3
owlowka <roksana.maria.duda@gmail.com>
2023-11-06 02:04:34 +0100

    Added user controller

---
+++ src/Chat.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Chat.DataAccess;
+using Chat.DataAccess.Entities;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chat.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IRepository<User> _userRepository;
+
+        public UserController(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        [Route("")]//https://localhost:80/user/
+        public IEnumerable<User> GetAllUsers() => _userRepository.GetAll();
+    }
+
+
+}
