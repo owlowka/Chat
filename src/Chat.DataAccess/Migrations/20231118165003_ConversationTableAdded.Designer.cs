cb441d84a81df3844ae246529fcc364a5bac2afa
owlowka <roksana.maria.duda@gmail.com>
2023-11-18 17:52:40 +0100

    Added `Conversation` migration

--- src/Chat.DataAccess/Migrations/20231118165003_ConversationTableAdded.Designer.cs
+++ src/Chat.DataAccess/Migrations/20231118165003_ConversationTableAdded.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Chat.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Chat.DataAccess.Migrations
 {
     [DbContext(typeof(ChatStorageContext))]
-    partial class ChatStorageContextModelSnapshot : ModelSnapshot
+    [Migration("20231118165003_ConversationTableAdded")]
+    partial class ConversationTableAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.13");
---
+++ src/Chat.DataAccess/Migrations/20231118165003_ConversationTableAdded.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chat.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConversationTableAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Conversations",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Conversations");
+        }
+    }
+}
