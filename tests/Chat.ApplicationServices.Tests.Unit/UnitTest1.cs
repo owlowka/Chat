5d151021c83673f88652fd069c5894bf498a4474
owlowka <roksana.maria.duda@gmail.com>
2024-02-21 16:12:17 +0100

    Added `Tests.Unit` projects for `ApplicationServices`, `DataAccess`, `Domain` and `WebAPI`

---
+++ tests/Chat.ApplicationServices.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Chat.ApplicationServices.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ tests/Chat.DataAccess.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Chat.DataAccess.Tests.Unit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
