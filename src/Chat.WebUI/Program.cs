12935fe75edbfbcfb9a95ce1a0d9ec5dfc83e558
owlowka <roksana.maria.duda@gmail.com>
2024-03-04 21:59:21 +0100

    Added `Mricrosoft.FluentUI.AspNetCore.Components`

--- src/Chat.WebUI/Program.cs
+++ src/Chat.WebUI/Program.cs
@@ -1,8 +1,13 @@
 using Chat.WebUI;
+using Chat.WebUI.Services;
 using Chat.WebUI.Services.Contracts;
 
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+using IMessageService = Chat.WebUI.Services.Contracts.IMessageService;
+using MessageService = Chat.WebUI.Services.MessageService;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +15,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7011/") });
 builder.Services.AddScoped<IMessageService, MessageService>();
+builder.Services.AddFluentUIComponents();
+
 
 await builder.Build().RunAsync();
