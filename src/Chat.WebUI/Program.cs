9795b89718b95217c56c22e4f8b318c4094e713a
owlowka <roksana.maria.duda@gmail.com>
2024-03-22 19:03:12 +0100

    Added authentication to `Chat.WebUI`

--- src/Chat.WebUI/Program.cs
+++ src/Chat.WebUI/Program.cs
@@ -3,7 +3,6 @@ using Chat.WebUI;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
-
 using IChatService = Chat.WebUI.Services.Contracts.IChatService;
 using HttpChatService = Chat.WebUI.Services.HttpChatService;
 
@@ -11,10 +10,19 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+//builder.Services.AddScoped(sp =>
+//new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
 builder.Services.AddScoped(sp =>
     new HttpClient { BaseAddress = new Uri("https://localhost:7011/") });
 builder.Services.AddScoped<IChatService, HttpChatService>();
 builder.Services.AddFluentUIComponents();
 
+builder.Services.AddOidcAuthentication(options =>
+{
+    // Configure your authentication provider options here.
+    // For more information, see https://aka.ms/blazor-standalone-auth
+    builder.Configuration.Bind("Local", options.ProviderOptions);
+});
 
 await builder.Build().RunAsync();
