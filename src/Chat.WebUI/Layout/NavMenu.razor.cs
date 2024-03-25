41e55b4c80f0cbe18c1815f2ab6955b22cf2e70a
owlowka <roksana.maria.duda@gmail.com>
2024-03-25 13:17:39 +0100

    Refactored `MainLayout` and `NavMenu`

---
+++ src/Chat.WebUI/Layout/NavMenu.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Chat.WebUI.Layout
+{
+    public class NavMenuBase : ComponentBase
+    {
+        protected bool _collapseNavMenu = true;
+
+        protected string? NavMenuCssClass => _collapseNavMenu ? "collapse" : null;
+
+        protected void ToggleNavMenu()
+        {
+            _collapseNavMenu = !_collapseNavMenu;
+        }
+    }
+}
