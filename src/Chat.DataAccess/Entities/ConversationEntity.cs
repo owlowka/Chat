cbad23023973c1ff6d661ac5d264dffc43221aa0
owlowka <roksana.maria.duda@gmail.com>
2024-04-08 22:16:16 +0200

    Removed redundant `Id` property from `ConversationEntity`

--- src/Chat.DataAccess/Entities/ConversationEntity.cs
+++ src/Chat.DataAccess/Entities/ConversationEntity.cs
@@ -3,7 +3,6 @@ namespace Chat.DataAccess.Entities
 {
     public class ConversationEntity : EntityBase
     {
-        public Guid Id { get; set; }
         public string Name { get; set; }
         public required ICollection<MessageEntity> Messages { get; init; }
         public required ICollection<UserEntity> Users { get; init; }
