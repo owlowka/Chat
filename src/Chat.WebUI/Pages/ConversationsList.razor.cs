ff0b0a151c0bfea42ec932377376c71b562c19a2
owlowka <roksana.maria.duda@gmail.com>
2024-04-08 12:25:58 +0200

    Added `Conversations`  page with search component

---
+++ src/Chat.WebUI/Pages/ConversationsList.razor.cs
@@ -0,0 +1,12 @@
+﻿using Chat.Domain.Conversation;
+using Chat.Domain.User;
+
+using Microsoft.AspNetCore.Components;
+
+namespace Chat.WebUI.Pages
+{
+    public class ConversationsListBase : ComponentBase
+    {
+        protected string _value;
+    }
+}
