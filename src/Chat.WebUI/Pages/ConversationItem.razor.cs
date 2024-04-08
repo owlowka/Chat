f9dde08f4bde127048c246c9982f7bc561478433
owlowka <roksana.maria.duda@gmail.com>
2024-04-08 22:02:34 +0200

    Added `ConversationItem` and `ConversationsList` component

---
+++ src/Chat.WebUI/Pages/ConversationItem.razor.cs
@@ -0,0 +1,14 @@
+﻿using Chat.Domain.Conversation;
+
+using Microsoft.AspNetCore.Components;
+
+namespace Chat.WebUI.Pages
+{
+    public class ConversationItemBase : ComponentBase
+    {
+        [Parameter]
+        public required ConversationModel Model { get; set; }
+
+
+    }
+}
---
+++ src/Chat.WebUI/Pages/ConversationsList.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using Chat.WebUI.Services.Contracts;
+
+using Chat.Domain.Conversation;
+
+namespace Chat.WebUI.Pages
+{
+    public class ConversationsListBase : ComponentBase
+    {
+        [Parameter]
+        public required List<ConversationModel>? List { get; set; }
+
+        protected bool _refreshing = false;
+
+        [Inject]
+        private IChatService ConversationService { get; set; }
+
+        public async Task RefreshConversationsAsync()
+        {
+            _refreshing = true;
+
+            try
+            {
+                IEnumerable<ConversationModel> conversations = await ConversationService.GetConversations();
+
+                List = conversations.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                _refreshing = false;
+            }
+
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await RefreshConversationsAsync();
+        }
+    }
+}
