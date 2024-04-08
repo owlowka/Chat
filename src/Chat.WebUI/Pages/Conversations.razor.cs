dba47b170e1f2a606eb20b2e690e6c72597387cb
owlowka <roksana.maria.duda@gmail.com>
2024-04-08 21:57:37 +0200

    Renamed `ConversationList` to `Conversations`

---
+++ src/Chat.WebUI/Pages/Conversations.razor.cs
@@ -0,0 +1,28 @@
+﻿using Chat.Domain.Conversation;
+using Chat.Domain.User;
+using Chat.WebUI.Services.Contracts;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace Chat.WebUI.Pages
+{
+    public class ConversationsBase : ComponentBase
+    {
+        protected FluentSearch? _searchTest;
+
+        protected string? _searchValue = string.Empty;
+
+        public ConversationModel Conversation { get; set; }
+
+        public IEnumerable<ConversationModel> Conversations { get; set; }
+
+        [Inject]
+        public IChatService HttpChatService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Conversations = await HttpChatService.GetConversations();
+        }
+    }
+}
