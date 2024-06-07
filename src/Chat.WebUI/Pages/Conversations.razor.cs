4d8d0089ff1d1ae9e82f831f54865cc19a0cda2c
owlowka <roksana.maria.duda@gmail.com>
2024-06-07 12:44:46 +0200

    Modified `Conversations` component

--- src/Chat.WebUI/Pages/Conversations.razor.cs
+++ src/Chat.WebUI/Pages/Conversations.razor.cs
@@ -1,16 +1,24 @@
-﻿using Chat.Domain.Conversation;
-using Chat.Domain.User;
+﻿using System.Security.Claims;
+
+using Chat.Domain.Conversation;
+
 using Chat.WebUI.Services.Contracts;
 
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.FluentUI.AspNetCore.Components;
 
 namespace Chat.WebUI.Pages
 {
     public class ConversationsBase : ComponentBase
     {
+        [CascadingParameter]
+        private Task<AuthenticationState> AuthenticationState { get; set; }
+
+        private ClaimsPrincipal? AuthenticatedUser { get; set; }
+
         [Parameter]
-        public string? UserName { get; set; } = "Roksana Duda";
+        public string? UserName { get; set; } /*= "Roksana Duda";*/
 
         protected FluentSearch? _searchTest;
 
@@ -20,13 +28,49 @@ namespace Chat.WebUI.Pages
 
         public List<ConversationModel> Conversations { get; set; }
 
+        List<string> _searchData;
+
         [Inject]
-        public IChatService HttpChatService { get; set; }
+        public IChatService ConversationsListService { get; set; }
+
+
+        protected List<string> _searchResults = DefaultResults();
+
+        protected static string _defaultResultsText = "no results";
+        private static List<string> DefaultResults()
+        {
+            return new() { _defaultResultsText };
+        }
+
+        public void HandleSearchInput()
+        {
+            if (string.IsNullOrWhiteSpace(_searchValue))
+            {
+                _searchResults = DefaultResults();
+                _searchValue = string.Empty;
+            }
+            else
+            {
+                string searchTerm = _searchValue.ToLower();
+
+                if (searchTerm.Length > 0)
+                {
+                    List<string> temp = _searchData
+                        .Where(str => str.ToLower().Contains(searchTerm))
+                        .Select(str => str).ToList();
+                }
+            }
+        }
 
         protected override async Task OnInitializedAsync()
         {
             //username from autentication as parameter
-            Conversations = (await HttpChatService.GetConversationsForUserName(UserName)).ToList();
+            AuthenticatedUser = (await AuthenticationState).User;
+            UserName ??= AuthenticatedUser.Identity?.Name;
+            Conversations = (await ConversationsListService.GetConversationsForUserName(UserName)).ToList();
+            _searchData = Conversations
+                .Select(conv => conv.Name)
+                .ToList();
         }
     }
 }
